a83f38bcd36e94e07012fcf8e1f2140a8186d239
Sameer M Nadaf <143956217+sameernadaf21@users.noreply.github.com>
2025-10-23 22:03:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	// Root endpoint
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to Go Gin Backend 🚀",
+			"message": "Welcome to Go Gin My Backend 🚀",
 		})
 	})
 
